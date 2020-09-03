57445765a6d65fc573dd45e4d9c83e83eddb1dcc
Joris <j_d_derks@hotmail.com>
2020-09-03 21:59:43 +0200

    Add content to gamemanager
    gamestate

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 /// <summary>
 /// This is the only monobehaviour in the game
 /// </summary>
+
+public enum GameState
+{
+    IS_PLAYING = 0,
+    IS_PAUSED = 1,
+    IS_GAMEOVER = 2 
+}
+
 public class GameManager : MonoBehaviour
 {
+    /// <summary> This is the state the game is currently in </summary>
+    public static GameState CURRENT_GAME_STATE { get; set; }
+
     void Start()
     {
-        
+        CURRENT_GAME_STATE = GameState.IS_PLAYING;
     }
     
     void Update()
