66b975c088595dec6e153c51aa48fdb17d3fd9d6
Joris <j_d_derks@hotmail.com>
2020-09-03 21:33:07 +0200

    Add GameManager
    adds GameManager, the only monobehaviour in our project.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// This is the only monobehaviour in the game
+/// </summary>
+public class GameManager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
