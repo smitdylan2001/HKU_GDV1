3bd1f13ce841cad57be08aac5c895db21b0a6e23
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-09-04 14:38:27 +0200

    Add Player Members and Functionality.
    Add Members such as Thrust Power and Rotation Power.
    Add Functionality to the Thrust and Rotation Functions.

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,34 +1,66 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+/// <summary>
+/// The Player Class holds all the members and functionality for the Player Object.
+/// It does not 
+/// </summary>
 public class Player
 {
 	/// <summary> The Health member of the player. </summary>
 	public int _health { get; private set; }
-	/// <summary> The Rotation member of the player </summary>
+	/// <summary> The Rotation member of the player. </summary>
 	public float _rotation { get; private set; }
+	/// <summary> The Thrusting Power of the player. </summary>
+	public float _thrustPower { get; private set; }
+	/// <summary> The Rotation Power of the player. </summary>
+	public float _rotationPower { get; private set; }
+
+	/// <summary> The sprite of the Player Gameobject. </summary>
+	public Sprite _sprite { get; private set; }
+
+	/// <summary> The Rigidbody2D Component of the Player. </summary>
+	public Rigidbody2D _rb2d { get; private set; }
+	/// <summary> The SpriteRenderer Component of the Player. </summary>
+	public SpriteRenderer _spriteRenderer { get; private set; }
 
 	/// <summary>
 	/// Constructor of the Player Class.
 	/// </summary>
 	/// <param name="health"> Starting Health of the player. </param>
 	/// <param name="rotation"> Starting Rotation of the player. </param>
-	public Player(int health, float rotation)
+	public Player(int health, float rotation, float thrustPower, float rotationPower, Sprite sprite)
 	{
 		_health = health;
 		_rotation = rotation;
+		_thrustPower = thrustPower;
+		_rotationPower = rotationPower;
+		_sprite = sprite;
+
+		GameObject playerGO = new GameObject();
+
+		playerGO.name = "Player";
+
+		playerGO.AddComponent<Rigidbody2D>();
+		_rb2d = playerGO.GetComponent<Rigidbody2D>();
+
+		playerGO.AddComponent<SpriteRenderer>();
+		_spriteRenderer = playerGO.GetComponent<SpriteRenderer>();
 	}
 
-	public void Thrust(KeyCode thrustKey)
+	/// <summary>
+	/// Applies Forward Thrust to the player.
+	/// </summary>
+	public void Thrust()
 	{
-		//TODO
-		// Implement thrusting code. (hehe)
+		_rb2d.AddForce(_rb2d.transform.up * _thrustPower * Time.deltaTime);
 	}
 
-	public void Rotate(KeyCode rotationKey)
+	/// <summary>
+	/// Rotates the Player on Input.
+	/// </summary>
+	/// <param name="rotationAxis"> The Rotation Axis. </param>
+	public void Rotate(float rotationAxis)
 	{
-		//TODO
-		// Implement Rotation.
+		_rb2d.MoveRotation(_rb2d.rotation -= rotationAxis * _rotationPower * Time.deltaTime);
 	}
 }
