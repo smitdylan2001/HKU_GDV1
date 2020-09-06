1fcf4d6e1713cbbc377ead107c0647ad02dadf09
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-09-06 17:35:51 +0200

    Fix Sprite loading. Add Resources Folder. Fix Player behaviour
    Fix Sprites not being loaded (Resources folder was missing).
    Add Resources Folder.
    Fix player shooting. Bullet's now actually travel toward the player direction.

--- Assets/Scripts/Projectiles/Bullet.cs
+++ Assets/Scripts/Projectiles/Bullet.cs
@@ -5,7 +5,7 @@ public class Bullet
 	/// <summary> Starting Position of the bullet. </summary>
 	public Vector2 _pos { get; private set; }
 	/// <summary> The rotation of the Bullet. (We use a float because only 1 axis has to be rotated.) </summary>
-	public float _rotation { get; private set; }
+	public Quaternion _rotation { get; private set; }
 	/// <summary> The speed at which the bullet travels. </summary>
 	public float _bulletSpeed { get; private set; }
 	/// <summary> The Sprite of the bullet. </summary>
@@ -16,22 +16,25 @@ public class Bullet
 	/// <summary> Reference to the SpriteRenderer Component. </summary>
 	public SpriteRenderer _spriteRenderer { get; private set; }
 
-	public Bullet(Vector2 pos = default, float rotation = 0f, float bulletSpeed = 300f, Sprite sprite = default)
+	public Bullet(Vector2 pos, Quaternion rotation, float bulletSpeed)
 	{
 		_pos = pos;
 		_rotation = rotation;
 		_bulletSpeed = bulletSpeed;
-		_sprite = sprite;
+
+		Sprite sprite = Resources.Load<Sprite>("Sprites/Bullet");
 
 		GameObject projectileGO = new GameObject();
+		projectileGO.transform.position = pos;
+		projectileGO.transform.rotation = rotation;
 		projectileGO.name = "Bullet";
 
 		Rigidbody2D rb2d = projectileGO.AddComponent<Rigidbody2D>();
 		_rb2d = rb2d;
-		_rb2d.SetRotation(rotation);
 		_rb2d.AddForce(_rb2d.transform.up * bulletSpeed);
 
 		SpriteRenderer spriteRenderer = projectileGO.AddComponent<SpriteRenderer>();
 		_spriteRenderer = spriteRenderer;
+		_spriteRenderer.sprite = sprite;
 	}
 }
