c4f5959ccff500d57cd3dd530501d87323a86bb3
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-09-21 23:25:20 +0200

    Fix Indentation. Fix Code conventions. Add Comments.

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -1,59 +1,57 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
 
 public static class UIManager
 {
 	/// <summary> This is the dictionary that keeps track of all the text UI elements in the game </summary>
-    public static Dictionary<string,TextMeshProUGUI> _uiTextElements = new Dictionary<string,TextMeshProUGUI>();
+	public static Dictionary<string, TextMeshProUGUI> UI_TEXT_ELEMENTS = new Dictionary<string, TextMeshProUGUI>();
 
 	/// <summary> This is the canvas on which the UI elements get put on </summary>
-    public static GameObject Canvas { get; set; }
+	public static GameObject Canvas { get; set; }
 
 	/// <summary>
 	/// This gets the main canvas in the scene
 	/// </summary>
-    public static void FindCanvas()
-    {
-        Canvas = GameObject.FindObjectOfType<Canvas>().gameObject;
-    }
+	public static void FindCanvas()
+	{
+		Canvas = GameObject.FindObjectOfType<Canvas>().gameObject;
+	}
 
-    /// <summary>
-    /// Generates a piece of text on a position given by the user
-    /// </summary>
-    /// <param name="position"></param>
-    /// <param name="text"></param>
-    /// <param name="editorName"></param>
-    /// <param name="parent"></param>
-    /// <param name="fontSize"></param>
-    public static void AddTextUIElement(Vector2 position ,string text, string editorName, Transform parent, float fontSize)
-    {
-        GameObject uiElementGO = new GameObject();
-        TextMeshProUGUI txt = uiElementGO.AddComponent<TextMeshProUGUI>();
-        RectTransform rTransform = uiElementGO.GetComponent<RectTransform>();
-        if (parent != null)
-        {
-            uiElementGO.transform.SetParent(parent.transform);
-            rTransform.localPosition = position;
-        }
-        else
-        {
-            Debug.LogError("UIManager can't add UI element without a canvas as its parent");
-        }
-        _uiTextElements.Add(editorName,txt);
+	/// <summary>
+	/// Generates a piece of text on a position given by the user
+	/// </summary>
+	/// <param name="position"></param>
+	/// <param name="text"></param>
+	/// <param name="editorName"></param>
+	/// <param name="parent"></param>
+	/// <param name="fontSize"></param>
+	public static void AddTextUIElement(Vector2 position, string text, string editorName, Transform parent, float fontSize)
+	{
+		GameObject uiElementGO = new GameObject();
+		TextMeshProUGUI txt = uiElementGO.AddComponent<TextMeshProUGUI>();
+		RectTransform rTransform = uiElementGO.GetComponent<RectTransform>();
+		if(parent != null)
+		{
+			uiElementGO.transform.SetParent(parent.transform);
+			rTransform.localPosition = position;
+		}
+		else
+		{
+			Debug.LogError("UIManager can't add UI element without a canvas as its parent");
+		}
+		UI_TEXT_ELEMENTS.Add(editorName, txt);
 		txt.fontSize = fontSize;
 		txt.text = text;
-    }
+	}
 
 	/// <summary>
 	/// Updates a text element with new text (for instance updating score)
 	/// </summary>
 	/// <param name="textMesh"></param>
 	/// <param name="newText"></param>
-    public static void UpdateUITextElement(TextMeshProUGUI textMesh, string newText)
-    {
-        textMesh.text = newText;
-    }
+	public static void UpdateUITextElement(TextMeshProUGUI textMesh, string newText)
+	{
+		textMesh.text = newText;
+	}
 }
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,23 +5,23 @@
 /// </summary>
 public class Player : ICollideable, IDamageable<int>
 {
-	/// <summary> The Health member of the player. </summary>
-	public int _health;
-	/// <summary> Size of the Player. </summary>
-	public float _size;
 	/// <summary> ICollideable HasCollided Implementation. </summary>
 	public bool HasCollided { get; set; }
-
-	/// <summary> Which Layers to check for collision. </summary>
-	public LayerMask CollisionMask { get; private set; }
-	/// <summary> The sprite of the Player Gameobject. </summary>
-	public Sprite Sprite { get; private set; }
-	/// <summary> The Rigidbody2D Component of the Player. </summary>
-	public SpriteRenderer SpriteRenderer { get; private set; }
-	/// <summary> The BoxCollider2D Component of the Bullet. </summary>
-	public BoxCollider2D BoxCollider2D { get; private set; }
 	/// <summary> The player gameobject for other classes to get</summary>
 	public GameObject PlayerGO { get; private set; }
+
+	/// <summary> The Health member of the player. </summary>
+	private int _health;
+	/// <summary> Size of the Player. </summary>
+	private float _size;
+	/// <summary> Which Layers to check for collision. </summary>
+	private LayerMask _collisionMask;
+	/// <summary> The sprite of the Player Gameobject. </summary>
+	private Sprite _sprite;
+	/// <summary> The Rigidbody2D Component of the Player. </summary>
+	private SpriteRenderer _spriteRenderer;
+	/// <summary> The BoxCollider2D Component of the Bullet. </summary>
+	private BoxCollider2D _boxCollider2D;
 
 	/// <summary>
 	/// Constructor of the Player Class.
@@ -31,21 +31,21 @@ public class Player : ICollideable, IDamageable<int>
 		_health = health;
 		_size = size;
 
-		Sprite = Resources.Load<Sprite>("Sprites/Player");
+		_sprite = Resources.Load<Sprite>("Sprites/Player");
 		PlayerGO = new GameObject();
 		PlayerGO.name = "Player";
 		PlayerGO.transform.localScale = new Vector3(_size, _size, _size);
 
 		BoxCollider2D boxCollider2D = PlayerGO.AddComponent<BoxCollider2D>();
-		BoxCollider2D = boxCollider2D;
-		BoxCollider2D.size = new Vector2(1, 1);
-		BoxCollider2D.isTrigger = true;
+		_boxCollider2D = boxCollider2D;
+		_boxCollider2D.size = new Vector2(1, 1);
+		_boxCollider2D.isTrigger = true;
 
 		SpriteRenderer spriteRenderer = PlayerGO.AddComponent<SpriteRenderer>();
-		SpriteRenderer = spriteRenderer;
-		SpriteRenderer.sprite = Sprite;
+		_spriteRenderer = spriteRenderer;
+		_spriteRenderer.sprite = _sprite;
 
-		CollisionMask = ~LayerMask.GetMask("Player", "Projectile");
+		_collisionMask = ~LayerMask.GetMask("Player", "Projectile");
 		PlayerGO.layer = LayerMask.NameToLayer("Player");
 
 		HasCollided = false;
@@ -56,13 +56,13 @@ public class Player : ICollideable, IDamageable<int>
 	/// </summary>
 	public bool IsColliding()
 	{
-		Collider2D[] collisions = Physics2D.OverlapCircleAll(PlayerGO.transform.position, _size, CollisionMask);
+		Collider2D[] collisions = Physics2D.OverlapCircleAll(PlayerGO.transform.position, _size, _collisionMask);
 
 		if(!HasCollided)
 		{
 			foreach(Collider2D collider in collisions)
 			{
-				if(collider != this.BoxCollider2D)
+				if(collider != this._boxCollider2D)
 				{
 					HasCollided = true;
 					return true;
